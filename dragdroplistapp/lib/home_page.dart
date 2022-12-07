2a11d25227b95ba2217b83a063498d0cf29e1d0e
Funda Demirtaş <demirtasfnd@gmail.com>
2022-12-07 09:32:21 +0300

    dragdroplistwiew
    - Drag Drop List View App

---
+++ dragdroplistapp/lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of tiles
+  final List myTiles = ['A', 'B', 'C', 'D'];
+  void updateMyTiles(int oldIndex, int newIndex) {
+    setState(() {
+      if (oldIndex < newIndex) {
+        newIndex--;
+      }
+      final tile = myTiles.removeAt(oldIndex);
+      myTiles.insert(newIndex, tile);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drag Drop List'),
+      ),
+      body: ReorderableListView(
+        children: [
+          for (final tile in myTiles)
+            ListTile(
+              key: ValueKey(tile),
+              title: Text(tile),
+            ),
+        ],
+        onReorder: (oldIndex, newIndex) => updateMyTiles(oldIndex, newIndex),
+      ),
+    );
+  }
+}
